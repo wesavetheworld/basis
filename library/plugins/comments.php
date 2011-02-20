80fe6049d650f2a63cf482f6d7aab0f55e2a5fd1
Dominic Giglio <humanshell@gmail.com>
2011-02-20 12:28:06 -0500

    moved delete_comment_link()

--- library/plugins/comments.php
+++ library/plugins/comments.php
@@ -15,8 +15,10 @@ function basis_comments($comment, $args, $depth) {
 			<header class="comment-author vcard">
 				<?php echo get_avatar($comment,$size='32',$default='<path_to_url>' ); ?>
 				<?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?>
-				<time><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf(__('%1$s'), get_comment_date(),  get_comment_time()) ?></a></time>
-				<?php edit_comment_link(__('(Edit)'),'  ','') ?>
+				<time>
+				  <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>"><?php printf(__('%1$s'), get_comment_date(),  get_comment_time()) ?></a>
+				</time>
+				<?php edit_comment_link(__('Edit '),'  ','') | delete_comment_link(get_comment_ID()); ?>
 			</header>
 
 			<?php if ($comment->comment_approved == '0') : ?>
@@ -28,7 +30,6 @@ function basis_comments($comment, $args, $depth) {
 			
 			<section class="comment_content clear">
 				<?php comment_text() ?>
-				<?php delete_comment_link(get_comment_ID()); ?>
 			</section>
 
 			<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
