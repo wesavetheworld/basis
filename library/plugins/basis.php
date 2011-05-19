7c45e84c7719adf3ad10ff0b9a1575f4433cc9ac
Dominic Giglio <humanshell@gmail.com>
2011-05-19 18:16:09 -0400

    updated custom search form

--- library/plugins/basis.php
+++ library/plugins/basis.php
@@ -79,11 +79,11 @@ add_filter('post_class', 'category_id_class');
 add_filter('body_class', 'category_id_class');
 
 // create custom search form
-function my_search_form( $form ) {
-  $form = '<form role="search" method="get" id="searchform" action="' . home_url( '/' ) . '" >
-  <div><label type="hidden" class="screen-reader-text" for="s">' . __('Search for:') . '</label>
-  <input type="text" value="' . get_search_query() . '" name="s" id="s" />
-  <input type="submit" id="searchsubmit" class="button" value="'. esc_attr__('Search') .'" />
+function my_search_form($form) {
+  $form = '<form role="search" method="get" id="searchform" action="'.home_url('/').'">
+  <div>
+  <input type="text" value placeholder="Search this blog..." name="s" id="s" />
+  <input type="submit" id="searchsubmit" class="button" value="'.esc_attr__('Search').'" />
   </div>
   </form>';
 
