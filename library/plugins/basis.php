d18c709cfc9d948e5b4cdee63c006e1ce73ecc98
Dominic Giglio <humanshell@gmail.com>
2011-06-20 22:24:36 -0400

    updated and reorganized

--- library/plugins/basis.php
+++ library/plugins/basis.php
@@ -2,33 +2,40 @@
 
 /**
 *
-* This is the core Basis functions file. In here we will enable or disable the
+* This is the core Basis functions file. This is where we enable or disable the
 * more "general" WordPress related features. A lot of this functionality comes
 * from the excellent work of Chris Coyier and Jeff Starr at http://digwp.com
 *
-* 1.) Add theme support for RSS Feeds, Post Thumbnails & Post Formats
-* 2.) Remove junk from the head section
-* 3.) Remove version info from all pages and feeds
-* 4.) Insert favicon link the head section
-* 5.) Include jQuery from Google
-* 6.) Enable threaded comments
-* 7.) Insert category id in body_class() and post_class()
-* 8.) Create custom search form
-* 9.) Brand the text in the footer of all admin pages
-*
 */
 
-// add feed links to the head section of every page
-add_theme_support('automatic-feed-links');
 
-// and enable post thumbnail support
+/**
+ *
+ * Include specific theme functionality
+ *
+ * post thumbnails
+ * feed links to the head section
+ * post formats
+ *
+ */
 add_theme_support('post-thumbnails');
+add_theme_support('automatic-feed-links');
+add_theme_support('post-formats', array('aside', 'quote', 'gallery'));
 
-// enable post format support
-add_theme_support( 'post-formats', array( 'aside', 'gallery' ) );
 
-// remove the junk from the head section
-function basis_remove_head_junk() {
+/**
+ *
+ * Remove automatically generated info for security
+ *
+ * detailed login error info
+ * all version info
+ * junk from the head section
+ *
+ */
+add_filter('login_errors', create_function('$clear_login_errors', "return null;"));
+add_filter('the_generator', create_function('$clear_the_generator', "return null;"));
+add_action('init', 'remove_head_junk');
+function remove_head_junk() {
   remove_action('wp_head', 'rsd_link');
   remove_action('wp_head', 'index_rel_link');
   remove_action('wp_head', 'wlwmanifest_link');
@@ -36,50 +43,80 @@ function basis_remove_head_junk() {
   remove_action('wp_head', 'parent_post_rel_link', 10, 0);
   remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
 }
-add_action('init', 'basis_remove_head_junk');
 
-// remove all version info from head and feeds
-function complete_version_removal() {
-	return '';
-}
-add_filter('the_generator', 'complete_version_removal');
 
-// insert favicon link in the head section
-function basis_favicon() {
-    echo '<link rel="shortcut icon" type="image/x-icon" href="';
-     echo bloginfo('stylesheet_directory');
-    echo '/library/images/favicon.ico" />';
-}
-add_action('wp_head', 'basis_favicon');
-
-// include jquery the smart way
+/**
+ *
+ * Dynamically include JavaScript files
+ *
+ * jQuery
+ * Modernizr
+ * Plugins
+ * Threaded Comments
+ *
+ */
 if (!is_admin()) {
 	wp_deregister_script('jquery');
-	wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"), false);
-	wp_enqueue_script('jquery');
+	wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"));
+	wp_register_script('modernizr', get_template_directory_uri().'/library/js/modernizr-2.0.4.js');
+	wp_register_script('plugins', get_template_directory_uri().'/library/js/plugins.js');
+	wp_enqueue_script('jquery', true);
+	wp_enqueue_script('modernizr');
+	wp_enqueue_script('plugins', true);
+	if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1))
+		wp_enqueue_script('comment-reply');
 }
 
-// enable threaded comments (must come after jquery is loaded)
-function enable_threaded_comments() {
-	if (!is_admin()) {
-		if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1))
-			wp_enqueue_script('comment-reply');
-		}
-}
-add_action('get_header', 'enable_threaded_comments');
 
-// insert category id in body_class() and post_class()
+/**
+ *
+ * Dynamically include stylesheets in the head
+ *
+ * default style.css
+ *
+ */
+if (!is_admin()) {
+  wp_register_style('basis-reset-style', get_template_directory_uri().'/library/css/style.css');
+  wp_register_style('basis-default-style', get_template_directory_uri().'/style.css');
+  wp_register_style('formalize-me', get_template_directory_uri().'/library/css/formalize.css');
+  wp_enqueue_style('basis-reset-style');
+  wp_enqueue_style('basis-default-style');
+  wp_enqueue_style('formalize-me');
+}
+
+
+/**
+ *
+ * Custom functionality and tweaks
+ *
+ * Insert favicon link in the head section
+ * Insert category id in body_class() and post_class()
+ * Create custom search form
+ * Brand the text in the footer of all admin pages
+ * Convert post time to fuzzy time
+ *
+ */
+add_action('wp_head', 'basis_favicon');
+add_filter('post_class', 'category_id_class');
+add_filter('body_class', 'category_id_class');
+add_filter('get_search_form', 'basis_search_form');
+add_filter('admin_footer_text', 'basis_admin_footer_text');
+add_filter('Posts', 'wp_fuzzy_time');
+
+function basis_favicon() {
+    echo '<link rel="shortcut icon" type="image/x-icon" href="';
+    echo bloginfo('stylesheet_directory');
+    echo '/library/images/favicon.ico" />';
+}
+
 function category_id_class($classes) {
 	global $post;
 	foreach((get_the_category($post->ID)) as $category)
 		$classes [] = 'cat-' . $category->cat_ID . '-id';
 		return $classes;
 }
-add_filter('post_class', 'category_id_class');
-add_filter('body_class', 'category_id_class');
 
-// create custom search form
-function my_search_form($form) {
+function basis_search_form($form) {
   $form = '<form role="search" method="get" id="searchform" action="'.home_url('/').'">
   <div>
   <input type="text" value placeholder="Search me..." name="s" id="s" />
@@ -88,12 +125,51 @@ function my_search_form($form) {
 
   return $form;
 }
-add_filter( 'get_search_form', 'my_search_form' );
 
-// brand the text in the footer of all admin pages
 function  basis_admin_footer_text() {
 	echo 'Theme designed by <a href="http://utadvisors.com">Universal Technology Advisors</a> using the Basis Theme Framework';
-} 
-add_filter('admin_footer_text', 'basis_admin_footer_text');
+}
+
+function wp_fuzzy_time() {
+	$postTime       = get_the_time("U");
+  $currentTime    = time();
+  $timeDifference = $currentTime - $postTime;
+  $minInSecs      = 60;
+  $hourInSecs     = 3600;
+  $dayInSecs      = 86400;
+  $monthInSecs    = $dayInSecs * 31;
+  $yearInSecs     = $dayInSecs * 366;
+    
+	if ($timeDifference > ($yearInSecs * 3))       // if over 3 years
+		$dateWithNiceTone = "quite a long while ago…";
+	else if ($timeDifference > ($yearInSecs * 2))  // if over 2 years
+		$dateWithNiceTone = "over two years ago";
+	else if ($timeDifference > $yearInSecs)        // if over a year 
+		$dateWithNiceTone = "over a year ago";
+  else if ($timeDifference > ($dayInSecs * 355)) // if over 355 days ago
+    $dateWithNiceTone = "around a year ago";
+  else if ($timeDifference > ($monthInSecs * 9)) // if over 9 months
+    $dateWithNiceTone = "almost a year ago";
+  else if ($timeDifference > ($monthInSecs * 6)) // if over 6 months
+    $dateWithNiceTone = "about half a year ago";
+	else if ($timeDifference > $monthInSecs)       // if over a month	
+	  $dateWithNiceTone = "more than a month ago";
+  else if ($timeDifference > ($dayInSecs * 28))  // if over 28 days ago
+	  $dateWithNiceTone = "around a month ago";
+	else if ($timeDifference >= ($dayInSecs * 8))  // if equal to or more than 8 days ago
+    $dateWithNiceTone = "in the last month";	    
+  else if ($timeDifference >= ($dayInSecs * 5))  // if equal to or more than 5 days ago
+	  $dateWithNiceTone = "around a week ago";        
+  else if ($timeDifference >= ($dayInSecs * 3))  // if equal to or more than 3 days ago
+	  $dateWithNiceTone = "a few days ago";
+  else if ($timeDifference >= ($dayInSecs * 2))  // if equal to or more than 2 days ago
+    $dateWithNiceTone = "a couple of days ago";   
+	else if ($timeDifference > $hourInSecs)        // if more than an hour ago
+		$dateWithNiceTone = "freshly baked";
+	else if ($timeDifference <= $hourInSecs)       // if less than or equal to an hour ago
+		$dateWithNiceTone = "just now";
+		
+  echo $dateWithNiceTone;
+}
 
 ?>
