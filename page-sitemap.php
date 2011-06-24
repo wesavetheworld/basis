83b286bbec4902c597a19d0b63297d1ddf6a00da
Dominic Giglio <humanshell@gmail.com>
2011-06-23 21:09:29 -0400

    alignment and layout updates

--- page-sitemap.php
+++ page-sitemap.php
@@ -6,54 +6,54 @@ Template Name: Sitemap Page
 
 get_header(); ?>
 			
-	<div id="content">
-	
-		<div id="main" role="main">
+<div id="content">
 
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div id="main" role="main">
+
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			
-  			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				
-  				<header>
-  					<h2 class="page-title"><?php the_title(); ?></h2>
-  				</header> <!-- end article header -->
+				<header>
+					<h2 class="page-title"><?php the_title(); ?></h2>
+				</header> <!-- end article header -->
+		
+				<section class="post_content">
+					<?php the_content(); ?>
+					<?php get_template_part('library/partials/sitemap'); ?>
+				</section> <!-- end article section -->
 			
-  				<section class="post_content">
-  					<?php the_content(); ?>
-  					<?php get_template_part('library/partials/sitemap'); ?>
-  				</section> <!-- end article section -->
-				
-  				<footer>
-  					<p class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ', ', ''); ?></p>
-  				</footer> <!-- end article footer -->
-			
-  			</article> <!-- end article -->
-						
-			<?php endwhile; ?>		
-			
-			<?php else : ?>
-			
-  			<article id="post-not-found">
-			  
-			    <header>
-			    	<h2>Not Found</h2>
-			    </header>
+				<footer>
+					<p class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ', ', ''); ?></p>
+				</footer> <!-- end article footer -->
+		
+			</article> <!-- end article -->
+					
+		<?php endwhile; ?>		
+		
+		<?php else : ?>
+		
+			<article id="post-not-found">
+		  
+		    <header>
+		    	<h2>Not Found</h2>
+		    </header>
+	    
+		    <section class="post_content">
+		    	<p>Sorry, but the requested resource was not found on this site.</p>
+		    </section>
+	    
+		    <footer>
+		    </footer>
 		    
-			    <section class="post_content">
-			    	<p>Sorry, but the requested resource was not found on this site.</p>
-			    </section>
-		    
-			    <footer>
-			    </footer>
-			    
-  			</article>
-			
-			<?php endif; ?>
-	
-		</div> <!-- end #main -->
+			</article>
+		
+		<?php endif; ?>
 
-		<?php get_sidebar(); // sidebar 1 ?>
+	</div> <!-- end #main -->
 
-	</div> <!-- end #content -->
+	<?php get_sidebar(); // sidebar 1 ?>
+
+</div> <!-- end #content -->
 
 <?php get_footer(); ?>
