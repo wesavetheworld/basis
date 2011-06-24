69be0728593c0aad78860192e5b7df8336ce1305
Dominic Giglio <humanshell@gmail.com>
2011-06-23 21:04:20 -0400

    alignment and layout updates

--- archive.php
+++ archive.php
@@ -9,77 +9,77 @@
 
 get_header(); ?>
 
-  <div id="content">
+<div id="content">
 
-    <div id="main" role="main">
+  <div id="main" role="main">
 
-      <?php if (is_category()) { ?>
-        <h3 class="archive_title"><span>Posts Categorized:</span> <?php single_cat_title(); ?></h3>
-      <?php } elseif (is_tag()) { ?> 
-        <h3 class="archive_title"><span>Posts Tagged:</span> <?php single_tag_title(); ?></h3>
-      <?php } elseif (is_author()) { ?>
-        <h3 class="archive_title"><span>Posts By:</span> <?php the_author_meta('display_name'); ?></h3>
-      <?php } elseif (is_day()) { ?>
-        <h3 class="archive_title"><span>Daily Archives:</span> <?php the_time('l, F j, Y'); ?></h3>
-      <?php } elseif (is_month()) { ?>
-        <h3 class="archive_title"><span>Monthly Archives:</span> <?php the_time('F Y'); ?></h3>
-      <?php } elseif (is_year()) { ?>
-        <h3 class="archive_title"><span>Yearly Archives:</span> <?php the_time('Y'); ?></h3>
-      <?php } ?>
+    <?php if (is_category()) { ?>
+      <h3 class="archive_title"><span>Posts Categorized:</span> <?php single_cat_title(); ?></h3>
+    <?php } elseif (is_tag()) { ?> 
+      <h3 class="archive_title"><span>Posts Tagged:</span> <?php single_tag_title(); ?></h3>
+    <?php } elseif (is_author()) { ?>
+      <h3 class="archive_title"><span>Posts By:</span> <?php the_author_meta('display_name'); ?></h3>
+    <?php } elseif (is_day()) { ?>
+      <h3 class="archive_title"><span>Daily Archives:</span> <?php the_time('l, F j, Y'); ?></h3>
+    <?php } elseif (is_month()) { ?>
+      <h3 class="archive_title"><span>Monthly Archives:</span> <?php the_time('F Y'); ?></h3>
+    <?php } elseif (is_year()) { ?>
+      <h3 class="archive_title"><span>Yearly Archives:</span> <?php the_time('Y'); ?></h3>
+    <?php } ?>
 
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <article id="post-<?php the_ID(); ?>">
+      <article id="post-<?php the_ID(); ?>">
 
-          <header>
-  				  <div id="post-img">
-              <?php if ( has_post_thumbnail() ) : ?>
-               <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-               <?php the_post_thumbnail('thumbnail'); ?>
-               </a>
-              <?php endif; ?>
-            </div>
-            <div id="post-preview">
-    					<h2>
-    					  <?php edit_post_link('✍','',' '); ?>
-    					  <a href="<?php the_permalink() ?>"
-    					     rel="bookmark"
-    					     title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-    					</h2>
-    					<p class="meta">
-                by <?php the_author_posts_link(); ?> on <time><?php the_time('F jS, Y'); ?></time><br />
-                in category <?php the_category(', '); ?> containing <?php word_count(); ?> words</p>
-            </div>
-  				</header> <!-- end article header -->
-
-          <section class="post_content">
-            <?php the_content('<span class="read-more">Read more on "'.the_title('', '', false).'" &raquo;</span>'); ?>
-          </section> <!-- end article section -->
-
-          <footer>
-            <?php if (has_tag()) : ?>
-              <p class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ', ', ''); ?></p>
-            <?php else : ?>
-              <p class="tags">Tags: [this post has not been tagged]</p>
+        <header>
+				  <div id="post-img">
+            <?php if ( has_post_thumbnail() ) : ?>
+             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+             <?php the_post_thumbnail('thumbnail'); ?>
+             </a>
             <?php endif; ?>
-          </footer> <!-- end article footer -->
+          </div>
+          <div id="post-preview">
+  					<h2>
+  					  <?php edit_post_link('✍','',' '); ?>
+  					  <a href="<?php the_permalink() ?>"
+  					     rel="bookmark"
+  					     title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+  					</h2>
+  					<p class="meta">
+              by <?php the_author_posts_link(); ?> on <time><?php the_time('F jS, Y'); ?></time><br />
+              in category <?php the_category(', '); ?> containing <?php word_count(); ?> words</p>
+          </div>
+				</header> <!-- end article header -->
 
-        </article> <!-- end article -->
+        <section class="post_content">
+          <?php the_content('<span class="read-more">Read more on "'.the_title('', '', false).'" &raquo;</span>'); ?>
+        </section> <!-- end article section -->
 
-      <?php endwhile; ?>	
+        <footer>
+          <?php if (has_tag()) : ?>
+            <p class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ', ', ''); ?></p>
+          <?php else : ?>
+            <p class="tags">Tags: [this post has not been tagged]</p>
+          <?php endif; ?>
+        </footer> <!-- end article footer -->
 
-      <?php if (function_exists('page_navi')) { // if expirimental feature is active ?>
+      </article> <!-- end article -->
 
-        <?php page_navi(); // use the page navi function ?>
+    <?php endwhile; ?>	
 
-      <?php } else { // if it is disabled, display regular wp prev & next links ?>
-        <nav class="wp-prev-next">
-          <ul>
-            <li class="prev-link"><?php next_posts_link('&laquo; Older Entries') ?></li>
-            <li class="next-link"><?php previous_posts_link('Newer Entries &raquo;') ?></li>
-          </ul>
-        </nav>
-      <?php } ?>
+    <?php if (function_exists('page_navi')) { // if expirimental feature is active ?>
+
+      <?php page_navi(); // use the page navi function ?>
+
+    <?php } else { // if it is disabled, display regular wp prev & next links ?>
+      <nav class="wp-prev-next">
+        <ul>
+          <li class="prev-link"><?php next_posts_link('&laquo; Older Entries') ?></li>
+          <li class="next-link"><?php previous_posts_link('Newer Entries &raquo;') ?></li>
+        </ul>
+      </nav>
+    <?php } ?>
 
 
     <?php else : ?>
@@ -99,7 +99,7 @@ get_header(); ?>
 
   </div> <!-- end #main -->
 
-  <?php get_sidebar(); // sidebar 1 ?>
+<?php get_sidebar(); // sidebar 1 ?>
 
 </div> <!-- end #content -->
 
