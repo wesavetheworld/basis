bb865399bd407c9c74f5bea12476bab069621cc4
Dominic Giglio <humanshell@gmail.com>
2011-06-23 21:08:53 -0400

    alignment and layout updates

--- page-custom.php
+++ page-custom.php
@@ -6,54 +6,54 @@ Template Name: Custom Page Example
 
 get_header(); ?>
 	
-	<div id="content">
-	
-		<div id="main" role="main">
+<div id="content">
 
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-			  <article id="post-<?php the_ID(); ?>">
-				
-  				<header>
-  				  <?php edit_post_link('✍','',' '); ?>
-  					<h2><?php the_title(); ?></h2>
-  				</header> <!-- end article header -->
+	<div id="main" role="main">
+
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		
-  				<section class="post_content">
-  					<?php the_content(); ?>
-  				</section> <!-- end article section -->
+		  <article id="post-<?php the_ID(); ?>">
 			
-  				<footer>
-  					<p><?php the_tags('<span class="tags">Tags: ', ', ', '</span>'); ?></p>
-  				</footer> <!-- end article footer -->
-			
-  			</article> <!-- end article -->
-			
-  		<?php endwhile; ?>	
-			
-  		<?php else : ?>
-			
-  			<article id="post-not-found">
-  			  
-  		    <header>
-  		    	<h2>Not Found</h2>
-  		    </header>
-		    
-  		    <section class="post_content">
-  		    	<p>Sorry, but the requested resource was not found on this site.</p>
-  		    </section>
-  		    
-  		    <footer>
-  		    </footer>
-  		    
-  			</article>
-			
-			<?php endif; ?>
+				<header>
+				  <?php edit_post_link('✍','',' '); ?>
+					<h2><?php the_title(); ?></h2>
+				</header> <!-- end article header -->
 	
-		</div> <!-- end #main -->
+				<section class="post_content">
+					<?php the_content(); ?>
+				</section> <!-- end article section -->
+		
+				<footer>
+					<p><?php the_tags('<span class="tags">Tags: ', ', ', '</span>'); ?></p>
+				</footer> <!-- end article footer -->
+		
+			</article> <!-- end article -->
+		
+		<?php endwhile; ?>	
+		
+		<?php else : ?>
+		
+			<article id="post-not-found">
+			  
+		    <header>
+		    	<h2>Not Found</h2>
+		    </header>
+	    
+		    <section class="post_content">
+		    	<p>Sorry, but the requested resource was not found on this site.</p>
+		    </section>
+		    
+		    <footer>
+		    </footer>
+		    
+			</article>
+		
+		<?php endif; ?>
 
-		<?php get_sidebar(); // sidebar 1 ?>
+	</div> <!-- end #main -->
 
-	</div> <!-- end #content -->
+	<?php get_sidebar(); // sidebar 1 ?>
+
+</div> <!-- end #content -->
 
 <?php get_footer(); ?>
