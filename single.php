ea2e986ac2e523b822ec8ff5c546d6ad9d95e059
Dominic Giglio <humanshell@gmail.com>
2011-06-23 22:39:37 -0400

    moved fancy edit pist link inside the h2 cause it wasn't showing up

--- single.php
+++ single.php
@@ -25,8 +25,7 @@ get_header(); ?>
             <?php endif; ?>
           </div>
           <div id="post-preview">
-            <?php edit_post_link('✍','',' '); ?>
-  					<h2><?php the_title(); ?></h2>
+  					<h2><?php edit_post_link('✍','',' '); ?><?php the_title(); ?></h2>
   					<p class="meta">
               by <?php the_author_posts_link(); ?> on <time><?php the_time('F jS, Y'); ?></time><br />
               in category <?php the_category(', '); ?> containing <?php word_count(); ?> words</p>
