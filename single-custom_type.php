4f90b2fa0dbb34f610e5d5666cb819b39770f22b
Dominic Giglio <humanshell@gmail.com>
2011-06-23 21:11:00 -0400

    alignment and layout updates

--- single-custom_type.php
+++ single-custom_type.php
@@ -16,60 +16,60 @@ get_header(); ?>
 
   <div id="main" role="main">
 
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-      <header>
-			  <div id="post-img">
-          <?php if ( has_post_thumbnail() ) : ?>
-           <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
-           <?php the_post_thumbnail('thumbnail'); ?>
-           </a>
+        <header>
+  			  <div id="post-img">
+            <?php if ( has_post_thumbnail() ) : ?>
+             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+             <?php the_post_thumbnail('thumbnail'); ?>
+             </a>
+            <?php endif; ?>
+          </div>
+          <div id="post-preview">
+  					<h2><?php the_title(); ?></h2>
+  					<p class="meta">
+              by <?php the_author_posts_link(); ?> on <time><?php the_time('F jS, Y'); ?></time><br />
+              in category <?php the_category(', '); ?> containing <?php word_count(); ?> words</p>
+          </div>
+  			</header> <!-- end article header -->
+
+        <section class="post_content">
+          <?php the_content(); ?>
+        </section> <!-- end article section -->
+
+        <footer>
+          <?php if (has_tag()) : ?>
+            <p class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ', ', ''); ?></p>
+          <?php else : ?>
+            <p class="tags">Tags: [this post has not been tagged]</p>
           <?php endif; ?>
-        </div>
-        <div id="post-preview">
-					<h2><?php the_title(); ?></h2>
-					<p class="meta">
-            by <?php the_author_posts_link(); ?> on <time><?php the_time('F jS, Y'); ?></time><br />
-            in category <?php the_category(', '); ?> containing <?php word_count(); ?> words</p>
-        </div>
-			</header> <!-- end article header -->
+        </footer> <!-- end article footer -->
 
-      <section class="post_content">
-        <?php the_content(); ?>
-      </section> <!-- end article section -->
+      </article> <!-- end article -->
 
-      <footer>
-        <?php if (has_tag()) : ?>
-          <p class="tags"><?php the_tags('<span class="tags-title">Tags:</span> ', ', ', ''); ?></p>
-        <?php else : ?>
-          <p class="tags">Tags: [this post has not been tagged]</p>
-        <?php endif; ?>
-      </footer> <!-- end article footer -->
+    <?php comments_template(); ?>
 
-    </article> <!-- end article -->
+    <?php endwhile; ?>			
 
-  <?php comments_template(); ?>
+    <?php else : ?>
 
-<?php endwhile; ?>			
+      <article id="post-not-found">
+        <header>
+          <h2>Not Found</h2>
+        </header>
+        <section class="post_content">
+          <p>Sorry, but the requested resource was not found on this site.</p>
+        </section>
+        <footer>
+        </footer>
+      </article>
 
-<?php else : ?>
+    <?php endif; ?>
 
-  <article id="post-not-found">
-    <header>
-      <h2>Not Found</h2>
-    </header>
-    <section class="post_content">
-      <p>Sorry, but the requested resource was not found on this site.</p>
-    </section>
-    <footer>
-    </footer>
-  </article>
-
-<?php endif; ?>
-
-</div> <!-- end #main -->
+  </div> <!-- end #main -->
 
 <?php get_sidebar(); // sidebar 1 ?>
 
