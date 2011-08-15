f951c74a56bd387446bced6d7a6577ba8e17dfa6
Dominic Giglio <humanshell@gmail.com>
2011-08-15 19:56:06 -0400

    removed breaks from comment form inputs

--- comments.php
+++ comments.php
@@ -80,15 +80,15 @@ if ( post_password_required() ) { ?>
 
             <ul id="comment-form-elements">
               <li>
-                <label for="author">Name <?php if ($req) echo "(required)"; ?></label><br />
+                <label for="author">Name <?php if ($req) echo "(required)"; ?></label>
                 <input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" placeholder="Your Name" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
               </li>
               <li>
-                <label for="email">Email <?php if ($req) echo "(required <small>will not be published</small>)"; ?></label><br />
+                <label for="email">Email <?php if ($req) echo "(required <small>will not be published</small>)"; ?></label>
                 <input type="email" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" placeholder="Your Email" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
               </li>
               <li>
-                <label for="url">Website</label><br />
+                <label for="url">Website</label>
                 <input type="url" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" placeholder="Your Website" tabindex="3" />
               </li>
             </ul>
